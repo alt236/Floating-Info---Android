f649b4723576f4be68d23fee2b64d3349cb238dc
Alexandros Schillings <aschillings@gmail.com>
2016-05-31 22:52:57 +0100

    Minor tidy in ProcessStore

--- app/src/main/java/uk/co/alt236/floatinginfo/data/access/generalinfo/inforeader/fgappinfo/ProcessStore.java
+++ app/src/main/java/uk/co/alt236/floatinginfo/data/access/generalinfo/inforeader/fgappinfo/ProcessStore.java
@@ -3,25 +3,39 @@ package uk.co.alt236.floatinginfo.data.access.generalinfo.inforeader.fgappinfo;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import eu.chainfire.libsuperuser.Shell;
 
 /*package*/ class ProcessStore {
+    public static final int INVALID_PID = -1;
+    private static final Pattern LINE_SPLIT_PATTERN = Pattern.compile("\\s+");
     private final Map<Integer, String> mPidToName = new HashMap<>();
     private final Map<String, Integer> mNameToPid = new HashMap<>();
+
+    private static int extractPid(final String[] line) {
+        return Integer.valueOf(line[1]);
+    }
+
+    private static String extractProcessName(final String[] line) {
+        return line[line.length - 1].split(":")[0];
+    }
 
     public void update() {
         mPidToName.clear();
         mNameToPid.clear();
 
         final List<String> stdout = Shell.SH.run("ps");
+
         int lineNo = 0;
+        String[] arr;
+        int pid;
+        String processName;
         for (final String line : stdout) {
-            if (lineNo > 0) {
-                // Get the process-name. It is the last column.
-                final String[] arr = line.split("\\s+");
-                final int pid = extractPid(arr);
-                final String processName = extractProcessName(arr);
+            if (lineNo > 0) { // the first line is the header
+                arr = LINE_SPLIT_PATTERN.split(line);
+                pid = extractPid(arr);
+                processName = extractProcessName(arr);
                 mPidToName.put(pid, processName);
                 mNameToPid.put(processName, pid);
             }
@@ -32,7 +46,7 @@ import eu.chainfire.libsuperuser.Shell;
     public int getPidFromName(final String name) {
         final Integer pid = mNameToPid.get(name);
         if (pid == null) {
-            return -1;
+            return INVALID_PID;
         } else {
             return pid;
         }
@@ -40,13 +54,5 @@ import eu.chainfire.libsuperuser.Shell;
 
     public String getNameFromPid(final int pid) {
         return mPidToName.get(pid);
-    }
-
-    private static int extractPid(final String[] line) {
-        return Integer.valueOf(line[1]);
-    }
-
-    private static String extractProcessName(final String[] line) {
-        return line[line.length - 1].split(":")[0];
     }
 }
